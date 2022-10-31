e35e253c931c952531097d0aec43df2137a02ba7
James Pace <pacej2022@gmail.com>
2022-10-31 08:47:29 -0700

    update sql

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO department (name);
+
+VALUES 
+        ("Managment"),
+        ("IT Department"),
+        ("Accounting"),
+        ("Administration"),
+        ("Human Resources");
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+        ("Project Manager", 80000, 1),
+        ("Senior Engineer", 100000, 2),
+        ("Engineer", 560000, 2),
+        ("Accountant", 560000, 3),
+        ("Admin Assistant", 350000, 4),
+        ("Recruiter", 70000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+
+        ("Ichigo", "Kurosaki", 2, NULL),
+        ("Orihime", "Inoue", 1, 1),
+        ("Rukia", "Kuchiki", 4, NULL);
