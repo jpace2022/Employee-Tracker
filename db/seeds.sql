88803921a9fd957ad660a1fab8b2fe6709015b07
James Pace <pacej2022@gmail.com>
2022-11-02 07:54:54 -0700

    updates

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,14 +11,16 @@ INSERT INTO role (title, salary, department_id)
 VALUES
         ("Project Manager", 80000, 1),
         ("Senior Engineer", 100000, 2),
-        ("Engineer", 560000, 2),
-        ("Accountant", 560000, 3),
-        ("Admin Assistant", 350000, 4),
-        ("Recruiter", 70000, 5);
+        ("Engineer", 50000, 2),
+        ("Accountant", 90000, 3),
+        ("Admin Assistant", 73500, 4),
+        ("Recruiter", 78300, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
 
         ("Ichigo", "Kurosaki", 2, NULL),
         ("Orihime", "Inoue", 1, 1),
-        ("Rukia", "Kuchiki", 4, NULL);
+        ("Rukia", "Kuchiki", 4, NULL),
+        ("Renji", "Abarai", 4, NULL),
+        ("Kenpachi", "Zaraki", 4, NULL);
